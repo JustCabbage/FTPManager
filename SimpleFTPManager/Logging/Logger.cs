d1d103fd6e8b4b0dbdc8edcb120880f9ee47e8bf
Cabbage <arjunjuneja4u@gmail.com>
2021-06-01 01:44:29 +0100

    Fixed invalid logging message.

--- SimpleFTPManager/Logging/Logger.cs
+++ SimpleFTPManager/Logging/Logger.cs
@@ -22,8 +22,8 @@ namespace SimpleFTPManager.Logging
         private static Dictionary<string, string> Messages = new Dictionary<string, string>() { { "UNABLE_TO_CONNECT", $"Unable to connect to server with these details. Please try again." },
                                                                                                 { "SUCCESSFUL_CONNECTION", $"Successfully connected to Server: {FTPManager.IP} on Port: {FTPManager.PORT}" }, 
                                                                                                 { "SUCCESSFUL_CREATE_DIR", "Successfully created Directory {0} on Server: {1}" },
-                                                                                                { "SUCCESSFUL_REMOVE_DIR", "Successfully created Directory {0} on Server: {1}" },
-                                                                                                { "DIRECTORY_ALREADY_EXISTS", "This directory already exists! Please try again." },
+                                                                                                { "SUCCESSFUL_REMOVE_DIR", "Successfully removed Directory {0} on Server: {1}" },
+                                                                                                { "DIRECTORY_ALREADY_EXISTS", "This directory already exists or permission is denied! Please try again." },
             };
     }
 
